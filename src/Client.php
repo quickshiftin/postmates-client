fa37b19099b3aa7503c201f5b48283c70ed5e102
Agli Panci <agli.panci@hotmail.com>
2017-02-15 14:37:13 +0100

    Revert "allow calling code to handle exceptions"
    This reverts commit 885a7e9c02213477af9cef4911c71d2f83eb6678.

--- src/Client.php
+++ src/Client.php
@@ -18,8 +18,6 @@ class Client extends \GuzzleHttp\Client
 
     private $_sCustomerId;
 
-    private $requestExceptions;
-
     public function __construct(array $config=[])
     {
         // Validate Postmates config values, these are required for the Postmates Client
@@ -186,23 +184,18 @@ class Client extends \GuzzleHttp\Client
             $oRsp = $this->send($oRq);
             return Factory::create($oRsp->json());
         } catch(\GuzzleHttp\Exception\RequestException $e) {
-            $this->addException($e);
+            echo $e->getRequest() . "\n";
+            if($e->hasResponse()) {
+                  echo 'HTTP request URL: ' . $e->getRequest()->getUrl() . "\n";
+                  /*
+                  echo 'HTTP request: ' . $e->getRequest() . "\n";
+                  echo 'HTTP response status: ' . $e->getResponse()->getStatusCode() . "\n";
+                  echo 'HTTP response: ' . $e->getResponse() . "\n";
+                */
+                echo $e->getResponse() . "\n";
+            }
         } catch(\Exception $e) {
-            $this->addException($e);
+            echo 'Uh oh! ' . $e->getMessage();
         }
-    }
-
-    private function addException($e) 
-    {
-        $this->requestExceptions[] = $e;
-    }
-
-    /**
-     * Retrieve an array of exceptions, if any were thrown during the request.
-     * See http://docs.guzzlephp.org/ for methods available with Guzzle request exceptions.
-     */
-    public function getRequestExceptions() 
-    {
-        return $this->requestExceptions;
     }
 }
